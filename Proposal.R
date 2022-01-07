f46fa8a2df6b5af119c533c94e82219bd6fe29f3
Han Havinga <h.m.havinga@gmail.com>
2022-01-07 09:34:58 +0100

    Beginnentje proposal

---
+++ Proposal.R
@@ -0,0 +1,49 @@
+# Basic functions (first priority)
+
+# - Read data (and set WD)
+
+# Preprocessing:
+#   - Anonymize data
+#   - Remove rows that don't have UserId or factId
+#   - Change RT to seconds?
+
+# Descriptive data stats:
+#   - Number of participants
+#   - Total Number of unique facts
+#   - Amount of unique lessons
+#   - Amount of unique participants per lesson
+#   - Amount of lessons for participants
+#   - Amount of sessions per participants
+#   - Session per lesson per participant?
+
+# Average session stats (data + plots):
+#   - Session duration
+#   - Number of trials
+#   - Number of unique facts
+#   - Accuracy
+#   - Reaction time
+
+# Individual overview stats (data + scatter/line plots):
+#   - Session duration
+#   - Number of trials
+#   - Number of unique facts
+#   - Accuracy
+#   - Reaction time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
