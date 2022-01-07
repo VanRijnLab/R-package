71fa0998b7323b9d3944d33c5707dcf9014636ec
Han Havinga <h.m.havinga@gmail.com>
2022-01-07 15:12:54 +0100

    Proposal v1.0

--- Proposal.R
+++ Proposal.R
@@ -1,3 +1,6 @@
+# Proposal R package SlimStampen
+
+
 # Basic functions (first priority)
 
 # - Read data (and set WD)
@@ -5,7 +8,6 @@
 # Preprocessing:
 #   - Anonymize data
 #   - Remove rows that don't have UserId or factId
-#   - Change RT to seconds?
 
 # Descriptive data stats:
 #   - Number of participants
@@ -22,18 +24,57 @@
 #   - Number of unique facts
 #   - Accuracy
 #   - Reaction time
+#   - Repetition of facts
 
-# Individual overview stats (data + scatter/line plots):
+# Individual overview stats (data + scatter/line plots) (option: to show individual):
 #   - Session duration
 #   - Number of trials
 #   - Number of unique facts
 #   - Accuracy
 #   - Reaction time
-
-# new line 
-# laptop access
+#   - Repetition of facts
 
 
+# RT/accuracy functions (data + plot)
+#   - Distribution RT/accuracy over all facts
+#   - RT/accuracy over duration of session
+#   - RT/accuracy over repetition of an item
+
+# Rate of Forgetting (data + plot):
+#   - General distribution ROF (average)
+#   - Final alpha over all facts
+#   - ROF over fact repetition for individual
+#   - ROF of one fact over all individuals
+
+
+
+
+
+# Extra features (Mixed priority)
+
+# - Example project with folder structure
+# - Cleaning data (many user options)
+# - R package in Github format
+# - Extra functions
+
+
+
+
+#Extra functions (secondary priority)
+
+# Compare lessons/blocks on:
+#   - Final alpha's
+#   - Session duration
+#   - Number of trials
+#   - Number of unique facts
+#   - Accuracy
+#   - Reaction time
+#   - Repetition of facts
+
+# Compare test results:
+#   - Over all facts
+#   - Over all studied facts
+#   - Show correctly answered unstudied items
 
 
 
