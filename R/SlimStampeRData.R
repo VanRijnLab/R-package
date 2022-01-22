87e21cc661abc6908e8918715386f49934ab793e
Han Havinga <h.m.havinga@gmail.com>
2022-01-22 17:21:18 +0100

    Added help page for whole package

---
+++ R/SlimStampeRData.R
@@ -0,0 +1,13 @@
+#' SlimStampeRData: Analyzing SlimStampen App Data
+#'
+#' The SlimStampeRData package provides three categories of important functions:
+#' basic data descriptions, plotting column information and plotting calculated
+#' information.
+#'
+#' @section Basic data descriptions:
+#' These functions...
+#'
+#' @docType package
+#' @name SlimStampeRData
+NULL
+#> NULL
--- R/read_dataset.R
+++ R/read_dataset.R
@@ -1,6 +1,8 @@
-#' Read the raw data set into a data frame
+#' Read dataset
 #'
-#' @param file data set file
+#' \code{read_dataset} reads the raw data set into a data frame with correct
+#' settings
+#' @param file A csv file.
 #'
 #' @return data frame
 #' @export
