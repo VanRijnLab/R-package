455f1c2cd6c91d99318be1d2e240fe7542335291
Han Havinga <h.m.havinga@gmail.com>
2022-02-17 16:25:48 +0100

    Incorrect answers denoted by red markers and more explicit console messages

--- R/plot_RT_over_time.R
+++ R/plot_RT_over_time.R
@@ -1,11 +1,19 @@
 #' Plot RT over time
 #'
-#' \code{plot_RT_over_time} plots the RT of one session over time.
+#' \code{plot_RT_over_time} plots the RT of one session over time. Incorrect
+#' answers are denotes with a red marker.
+#'
 #' @param data A data frame. NA values will be removed before plotting.
-#' @param sessionId Provide a single sessionId string to plot that session. If sessionId is NULL all sessions will be plotted.
-#' @param normalizeTime If TRUE, the times of all facts will be normalized (they will start at 0). If FALSE, the times will not be normalized and data points will occur relative to their occurrence during the session.
-#' @param xlim A vector of 2 (for example: c(0, 10)), indicating the range of the x-axis.If NULL the default value is used: c(0, max(time)).
-#' @param ylim A vector of 2 (for example: c(0, 10)), indicating the range of the y-axis.If NULL the default value is used: c(min(RT), mean(RT) + (2*SD(RT))).
+#' @param sessionId Provide a single sessionId string to plot that session. If
+#'   sessionId is NULL all sessions will be plotted.
+#' @param normalizeTime If TRUE, the times of all facts will be normalized (they
+#'   will start at 0). If FALSE, the times will not be normalized and data
+#'   points will occur relative to their occurrence during the session.
+#' @param xlim A vector of 2 (for example: c(0, 10)), indicating the range of
+#'   the x-axis.If NULL the default value is used: c(0, max(time)).
+#' @param ylim A vector of 2 (for example: c(0, 10)), indicating the range of
+#'   the y-axis.If NULL the default value is used: c(min(RT), mean(RT) +
+#'   (2*SD(RT))).
 #' @param filepath A relative or explicit path where plots will be saved
 #' @return data frame
 #' @export
@@ -18,6 +26,19 @@ plot_RT_over_time <- function(data, sessionId = NULL, normalizeTime = FALSE, xli
   }
   if(!(is.null(ylim) | length(ylim) == 2)){
     stop("ylim must be a vector of 2")
+  }
+  indx <- apply(data, 2, function(x) any(is.na(x)))
+  if("reactionTime" %in% colnames(data)[indx]){
+    cat("! There are missing values in the column reactionTime. \n")
+  }
+  if("sessionTime" %in% colnames(data)[indx]){
+    cat("! There are missing values in the column sessionTime. \n")
+  }
+  if("factId" %in% colnames(data)[indx]){
+    cat("! There are missing values in the column factId. \n")
+  }
+  if("correct" %in% colnames(data)[indx]){
+    cat("! There are missing values in the column correct. \n")
   }
 
   if(is.null(sessionId)){
@@ -50,16 +71,20 @@ plot_RT_over_time <- function(data, sessionId = NULL, normalizeTime = FALSE, xli
     y = ylim
   }
 
-  # grDevices::pdf(file="../Figures/RT_plots.pdf")
-  # graphics::par(mfrow=c(2,2))
-
   facts <- unique(data$factId)
-  factcolor <- viridis::viridis(length(facts))
+  factcolor <- viridis::turbo(length(facts))
+  # factcolor <- grDevices::rainbow(length(facts))
   names(factcolor)  <- facts
+
+  corrections <- unique(data$correct)
+  corcolor <- c("grey", "red")
+  names(corcolor)  <- corrections
+
 
   cat("This may take a moment... \n")
   for (i in seq_along(participants)) {
     dat1 <- dplyr::filter(data, sessionId == participants[i])
+
     dat3 <- NULL
     if(normalizeTime){
       dat2 <- dplyr::group_by(dat1, factId)
@@ -76,12 +101,10 @@ plot_RT_over_time <- function(data, sessionId = NULL, normalizeTime = FALSE, xli
     # Make plot
     plot <- ggplot2::ggplot(data = dat3, ggplot2::aes(x = time, y = reactionTime)) +
       ggplot2::geom_line(alpha = 1, ggplot2::aes(colour = factor(factId))) +
-      ggplot2::geom_point(alpha = 0.5, size = 1, ggplot2::aes(colour = factor(factId), fill = factor(factId))) +
-      # ggplot2::geom_point(data = dat3[which(dat3$factId == 61033),],
-      #                     ggplot2::aes(size = 1), colour = "red") +
+      ggplot2::geom_point(alpha = 1, size = 1.5, stroke = 0, pch = 21, ggplot2::aes(fill = correct)) +
       ggplot2::guides(colour = "none", fill = "none") +
-      ggplot2::scale_color_viridis_d() +
-      ggplot2::scale_fill_manual(values = factcolor) +
+      ggplot2::scale_fill_manual(values = corcolor) +
+      ggplot2::scale_color_manual(values = factcolor) +
       ggplot2::coord_cartesian(xlim = x, ylim = y) +
       ggplot2::labs(x = "Time (minutes)", y = "Reaction Time (ms)") +
       ggplot2::ggtitle(plotTitle)
@@ -89,7 +112,6 @@ plot_RT_over_time <- function(data, sessionId = NULL, normalizeTime = FALSE, xli
     if(i < 5){
       plots4[[i]] <- plot
     }
-    # print(plot)
   }
   res <- cowplot::plot_grid(plotlist = plots4, nrow = 2, ncol = 2)
 
