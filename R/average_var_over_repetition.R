6d28677742eeee36c129639939282b00728db078
Han Havinga <h.m.havinga@gmail.com>
2022-02-18 14:54:18 +0100

    Seperate file for helper functions

--- R/average_var_over_repetition.R
+++ R/average_var_over_repetition.R
@@ -212,36 +212,4 @@ average_accuracy_over_repetition <- function(data, xlim = NULL, ylim = NULL, fil
   return(plot)
 }
 
-set_x <- function(column, xlim) {
-  maxTime <- max(column)
-  if(is.null(xlim)){
-    x = c(1, maxTime)
-  } else {
-    x = xlim
-  }
-  return(x)
-}
-
-set_y <- function(column, ylim) {
-  meancol <- mean(column, na.rm=TRUE)
-  sdcol <- stats::sd(column, na.rm=TRUE)
-  uppercol <- meancol + sdcol
-  lowercol <- min(column)
-  if(is.null(ylim)){
-    y = c(lowercol, uppercol)
-  } else {
-    y = ylim
-  }
-  return(y)
-}
-
-missing_values_message <- function(data, columns) {
-  indx <- apply(data, 2, function(x) any(is.na(x)))
-  for(col in columns){
-    if(col %in% colnames(data)[indx]){
-      cat("! There are missing values in the column ", col, " ! \n")
-    }
-  }
-}
-
 
