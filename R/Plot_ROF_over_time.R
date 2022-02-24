41836f860b94a1800252b0ea97efb550c1be4223
Han Havinga <h.m.havinga@gmail.com>
2022-02-24 11:46:37 +0100

    Init plot ROF over time

---
+++ R/Plot_ROF_over_time.R
@@ -0,0 +1,116 @@
+#' Plot ROF over time
+#'
+#' \code{plot_ROF_over_time} plots the rate of forgetting (alpha) of one session over time. Incorrect
+#' answers are denotes with a red marker.
+#'
+#' @param data A data frame. NA values will be removed before plotting.
+#' @param sessionId Provide a single sessionId string to plot that session. If
+#'   sessionId is NULL all sessions will be plotted.
+#' @param normalizeTime If TRUE, the times of all facts will be normalized (they
+#'   will start at 0). If FALSE, the times will not be normalized and data
+#'   points will occur relative to their occurrence during the session.
+#' @param xlim A vector of 2 (for example: c(0, 10)), indicating the range of
+#'   the x-axis.If NULL the default value is used: c(0, z). Where z is the max time.
+#' @param ylim A vector of 2 (for example: c(0, 10)), indicating the range of
+#'   the y-axis.If NULL the default value is used: c(0.10, 0.5).
+#' @param filepath A relative or explicit path where plots will be saved
+#' @return data frame
+#' @export
+plot_ROF_over_time <- function(data, sessionId = NULL, normalizeTime = FALSE, xlim = NULL, ylim = NULL, filepath = "../Figures") {
+  if(missing(data)){
+    stop("No data is provided")
+  }
+  if(!("alpha" %in% colnames(data))){
+    stop("No alpha column is provided in the data, run calculate_alpha_and_activation() to add an alpha column to the data")
+  }
+  if(!(is.null(xlim) | length(xlim) == 2)){
+    stop("xlim must be a vector of 2")
+  }
+  if(!(is.null(ylim) | length(ylim) == 2)){
+    stop("ylim must be a vector of 2")
+  }
+
+  missing_values_message(data, c("sessionId", "factId", "sessionTime", "correct", "alpha"))
+
+  if(is.null(sessionId)){
+    participants <- unique(data$sessionId)
+  } else {
+    if(!(is.character(sessionId) & length(sessionId) == 1)){
+      stop("SessionId is not a string")
+    }
+    participants <- character(0)
+    participants[1] <- sessionId
+  }
+
+  plot <- NULL
+  plots <- list()
+  plots4 <- list()
+
+  maxTime <- max(data$sessionTime)/60000
+  if(is.null(xlim)){
+    x = c(0, maxTime)
+  } else {
+    x = xlim
+  }
+  if(is.null(ylim)){
+    y = c(0.10, 0.5)
+  } else {
+    y = ylim
+  }
+
+  facts <- unique(data$factId)
+  factcolor <- viridis::turbo(length(facts))
+  names(factcolor)  <- facts
+
+  corrections <- unique(data$correct)
+  corcolor <- c("grey", "red")
+  names(corcolor)  <- corrections
+
+
+  cat("This may take a moment... \n")
+  for (i in seq_along(participants)) {
+    dat1 <- dplyr::filter(data, sessionId == participants[i])
+
+    dat3 <- NULL
+    if(normalizeTime){
+      dat2 <- dplyr::group_by(dat1, factId)
+      #when sorted by person, session and factId
+      dat3 <- dplyr::mutate(dat2, time = (sessionTime - min(sessionTime)) / 60000)
+      dat3 <- dplyr::ungroup(dat3)
+    } else {
+      dat3 <- dplyr::mutate(dat1, time = (sessionTime / 60000))
+    }
+    # Make plot title
+    lesson <- unique(dat3$lessonTitle)
+    user <- unique(dat3$userId)
+    plotTitle <- paste("Lesson: ", lesson[1], ", User: ", user[1])
+
+    # Make plot
+    plot <- ggplot2::ggplot(data = dat3, ggplot2::aes(x = time, y = alpha)) +
+      ggplot2::geom_line(alpha = 1, ggplot2::aes(colour = factor(factId))) +
+      ggplot2::geom_point(alpha = 1, size = 1.5, stroke = 0, pch = 21, ggplot2::aes(fill = correct)) +
+      ggplot2::guides(colour = "none", fill = "none") +
+      ggplot2::scale_fill_manual(values = corcolor) +
+      ggplot2::scale_color_manual(values = factcolor) +
+      ggplot2::coord_cartesian(xlim = x, ylim = y) +
+      ggplot2::labs(x = "Time (minutes)", y = "Alpha") +
+      ggplot2::ggtitle(plotTitle)
+    plots[[i]] <- plot
+    if(i < 5){
+      plots4[[i]] <- plot
+    }
+  }
+  res <- cowplot::plot_grid(plotlist = plots4, nrow = 2, ncol = 2)
+
+  # Save all plots to a pdf file
+  date <- format(Sys.time(), "%d-%b-%Y %Hh%Mm")
+  title <- paste("ROF_over_time_", date, ".pdf")
+  ggplot2::ggsave(title, gridExtra::marrangeGrob(grobs = plots, nrow=2, ncol=2),
+                  device = "pdf", path = filepath, width = 22, height = 22, units = "cm")
+
+  cat("Preview of the first 4 plots are displayed in viewer. \n")
+  cat("PDF of plots can be found in: ", filepath, "\n")
+
+  # Display first 4 plots
+  return(res)
+}
