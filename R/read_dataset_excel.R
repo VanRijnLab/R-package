3f348b46bc663cf0592a027c2bc3af76e6cebf65
Han Havinga <h.m.havinga@gmail.com>
2022-04-14 16:00:18 +0200

    Extracted functions, replaced json parser, exlude rows with incorrect json

--- R/read_dataset_excel.R
+++ R/read_dataset_excel.R
@@ -14,65 +14,23 @@ read_dataset_excel <- function(file_response = "../SlimStampen_data_examples/voc
   # if(file == system.file("extdata", "files.xlsx", package = "SlimStampeRData")) {
   #   message("Example dataset was used: excel sample")
   # }
-  cat("This may take a few minutes... \n")
+  cat("This may take a few minutes... \n Rows with missing data are removed. \n")
 
   out <- list()
 
-  out$dataLesson <- readxl::read_excel(file_lesson)
+  out$dataLesson <- readxl::read_excel(file_lesson, guess_max = 10000)
 
-  out$dataFact <- readxl::read_excel(file_fact)
+  out$dataFact <- readxl::read_excel(file_fact, guess_max = 10000)
 
-  out$dataResponse <- readxl::read_excel(file_response)
+  out$dataResponse <- readxl::read_excel(file_response, guess_max = 10000)
 
-  jsonToDataFrame <- function(string) {
-    # parsedJSON <- jsonlite::fromJSON(string)
-    parsedJSON <- jsonlite::fromJSON(txt=gsub("null", "\"\"", string))
-    # parsedJSON <- jsonlite::fromJSON(txt=gsub("null", "NA", string))
-    # parsedJSON <- do.call(c, jsonlite:::null_to_na(jsonlite::fromJSON(string)))
-    # parsedJSON <- jsonlite::fromJSON(txt=gsub("\"\"", "\"", string))
-    parsedJSON[[14]] = NULL
-    parsedJSON[[12]] = NULL
-    parsedJSON[[8]] = NULL
-    return(parsedJSON)
-  }
+  # Filter dataResponse on lessonId from input (tbd)
 
-  colBindJson <- function(dataFrame) {
-    colnames(dataFrame) <- c("userId", "factId_gen", "lessonId", "sequence_number",
-                             "data", "create_time")
+  #Gathering data
+  # Find lesson name
+  out$lessonResponse <- findLesson(out$dataResponse, out$dataLesson)
 
-    newList <- jsonToDataFrame(dataFrame$data[1])
-
-    # Create empty data from for json data
-    jsonData <- data.frame(matrix(NA, nrow = nrow(dataFrame), ncol = length(newList)))
-
-    # Set column headers from parsed json
-    colnames(jsonData) <- names(newList)
-
-    #Set column types for new dataframe
-
-    # read JSON and convert to data.frame row
-    for (rowNumber in 1:nrow(dataFrame)) {
-      cat("rownumber", rowNumber, "\n")
-      dataList <- jsonToDataFrame(dataFrame$data[rowNumber])
-      # dataDf <- as.data.frame(matrix(unlist(dataList),nrow=length(dataList),byrow=TRUE))
-      if(dataList$reactionTime == "") {dataList$reactionTime <- NA}
-      dataDf <- as.data.frame(do.call(rbind, dataList))
-      # Transpose the list (which is interpreted as a column), to a row
-      jsonData[rowNumber,] <- t(dataDf[1])
-    }
-    as.data.frame(lapply(jsonData, type.convert))
-
-    combinedDF <- cbind(dataFrame, jsonData)
-
-    return(combinedDF)
-  }
-
-  out$ResponseFinal <- colBindJson(out$dataResponse)
-
-
-
-
-
+  # out$ResponseFinal <- colBindJson(out$dataResponse)
 
 
   # cols <- c("factId", "userId", "sessionTime", "reactionTime", "correct", "lessonTitle", "lessonId","sessionId",
@@ -100,3 +58,70 @@ read_dataset_excel <- function(file_response = "../SlimStampen_data_examples/voc
 
   return(out)
 }
+
+jsonToDataFrame <- function(string) {
+  parsedJSON <- rjson::fromJSON(json_str =gsub("null", "\"\"", string))
+  # parsedJSON <- do.call(c, jsonlite:::null_to_na(jsonlite::fromJSON(string)))
+  parsedJSON[[14]] = NULL
+  parsedJSON[[12]] = NULL
+  parsedJSON[[8]] = NULL
+  return(parsedJSON)
+}
+
+colBindJson <- function(dataFrame) {
+  colnames(dataFrame) <- c("userId", "factId_gen", "lessonId", "sequence_number",
+                           "data", "create_time")
+
+  newList <- jsonToDataFrame(dataFrame$data[1])
+
+  # Create empty data from for json data
+  jsonData <- data.frame(matrix(NA, nrow = nrow(dataFrame), ncol = length(newList)))
+
+  # Set column headers from parsed json
+  colnames(jsonData) <- names(newList)
+
+  #Set column types for new dataframe
+  start_time <- format(Sys.time(), "%d-%b-%Y_%Hh%Mm%Ss")
+  cat(start_time, "\n")
+  counter = 0;
+
+  # read JSON and convert to data.frame row
+  for (rowNumber in 1:nrow(dataFrame)) {
+    # cat("rownumber", rowNumber, "\n")
+    if(rowNumber%%1000 == 0) {cat("rownumber", rowNumber, "\n")}
+
+    dataList <- jsonToDataFrame(dataFrame$data[rowNumber])
+
+    if(length(dataList) == length(newList)) {
+      if(dataList$reactionTime == "") {dataList$reactionTime <- NA}
+      dataDf <- as.data.frame(matrix(unlist(dataList),nrow=length(dataList),byrow=TRUE))
+      # dataDf <- as.data.frame(do.call(rbind, dataList))
+      # Transpose the list (which is interpreted as a column), to a row
+      jsonData[rowNumber,] <- t(dataDf[1])
+    } else {
+      counter = counter + 1
+    }
+  }
+  # as.data.frame(lapply(jsonData, type.convert))
+  jsonData <- as.data.frame(lapply(jsonData, type.convert, as.is = TRUE))
+
+  combinedDF <- cbind(dataFrame, jsonData)
+  end_time <- format(Sys.time(), "%d-%b-%Y_%Hh%Mm%Ss")
+  cat(end_time, "\n")
+  if(counter > 0){
+    cat(counter, "row(s) were removed. \n")
+  }
+
+  return(combinedDF)
+}
+
+findLesson <- function(dataframe, lessonInfo) {
+  # dataframe[nrow(dataframe) + 1,] = list(1, 1, 1900, 1, "data", "1")
+
+
+  dataframe$lessonTitle <- lessonInfo$title[match(dataframe$lesson_id, lessonInfo$id)]
+
+  return(dataframe)
+}
+
+
