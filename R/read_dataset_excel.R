eb00a942aece45cfeb2c3aa8a2d1287c559d4f29
Han Havinga <h.m.havinga@gmail.com>
2022-04-15 14:35:46 +0200

    Merged tables and renamed columns

--- R/read_dataset_excel.R
+++ R/read_dataset_excel.R
@@ -8,13 +8,15 @@
 #' @return data frame
 #' @export
 #'
-read_dataset_excel <- function(file_response = "../SlimStampen_data_examples/vocatrainer/small_response.xlsx",
+read_dataset_excel <- function(lessons = 27, file_response = "../SlimStampen_data_examples/vocatrainer/small_response.xlsx",
                                file_lesson = "../SlimStampen_data_examples/vocatrainer/nmd_live_vocatrainer_public_lesson.xlsx",
                                file_fact = "../SlimStampen_data_examples/vocatrainer/nmd_live_vocatrainer_public_fact.xlsx") {
   # if(file == system.file("extdata", "files.xlsx", package = "SlimStampeRData")) {
   #   message("Example dataset was used: excel sample")
   # }
   cat("This may take a few minutes... \n Rows with missing data are removed. \n")
+  start_time <- format(Sys.time(), "%d-%b-%Y_%Hh%Mm%Ss")
+  cat(start_time, "\n")
 
   out <- list()
 
@@ -24,37 +26,48 @@ read_dataset_excel <- function(file_response = "../SlimStampen_data_examples/voc
 
   out$dataResponse <- readxl::read_excel(file_response, guess_max = 10000)
 
+  renamedResponse = dplyr::rename(out$dataResponse, "userId" = "user_id", "factId_gen" = "fact_id", "lessonId" = "lesson_id")
+
   # Filter dataResponse on lessonId from input (tbd)
 
+  # Parse JSON
+  out$parsedResponse <- colBindJson(renamedResponse)
+
   #Gathering data
-  # Find lesson name
-  out$lessonResponse <- findLesson(out$dataResponse, out$dataLesson)
-
-  # out$ResponseFinal <- colBindJson(out$dataResponse)
+  lessonResponse <- findlesson(out$parsedResponse, out$dataLesson)
+  out$finalResponse <- findfact(lessonResponse, out$dataFact)
 
 
-  # cols <- c("factId", "userId", "sessionTime", "reactionTime", "correct", "lessonTitle", "lessonId","sessionId",
-  #           "factText")
-  # missingcol <- missing_columns_check(data, cols)
-  #
-  # if(length(missingcol) > 0){
-  #   strCols <- paste(cols,collapse=" ")
-  #   warning("One or more of these columns is missing: ", strCols,"\n Some of the funcions in this package may not work if these columns are not provided")
-  # }
+
+
+
+
+
+
+  cols <- c("factId", "userId", "sessionTime", "reactionTime", "correct", "lessonTitle", "lessonId","sessionId",
+            "factText")
+  missingcol <- missing_columns_check(out$finalResponse, cols)
+
+  if(length(missingcol) > 0){
+    strCols <- paste(cols,collapse=" ")
+    warning("One or more of these columns is missing: ", strCols,"\n Some of the funcions in this package may not work if these columns are not provided")
+  }
 
   # # Separate messages for alpha and repetition
   # colsalpha <- c("alpha")
-  # missingalpha <- missing_columns_check(data, colsalpha)
+  # missingalpha <- missing_columns_check(out$finalResponse, colsalpha)
   # if(length(missingalpha) > 0){
   #   cat("! No alpha column is provided in the data, run calculate_alpha_and_activation() to add an alpha column to the data \n")
   # }
   #
   # colsrep <- c("repetition")
-  # missingrep <- missing_columns_check(data, colsrep)
+  # missingrep <- missing_columns_check(out$finalResponse, colsrep)
   # if(length(missingrep) > 0){
   #   cat("! No repetition column is provided in the data, run calculate_repetition() to add a repetition column to the data \n")
   # }
 
+  end_time <- format(Sys.time(), "%d-%b-%Y_%Hh%Mm%Ss")
+  cat(end_time, "\n")
 
   return(out)
 }
@@ -69,9 +82,6 @@ jsonToDataFrame <- function(string) {
 }
 
 colBindJson <- function(dataFrame) {
-  colnames(dataFrame) <- c("userId", "factId_gen", "lessonId", "sequence_number",
-                           "data", "create_time")
-
   newList <- jsonToDataFrame(dataFrame$data[1])
 
   # Create empty data from for json data
@@ -80,9 +90,9 @@ colBindJson <- function(dataFrame) {
   # Set column headers from parsed json
   colnames(jsonData) <- names(newList)
 
-  #Set column types for new dataframe
-  start_time <- format(Sys.time(), "%d-%b-%Y_%Hh%Mm%Ss")
-  cat(start_time, "\n")
+
+  # start_time <- format(Sys.time(), "%d-%b-%Y_%Hh%Mm%Ss")
+  # cat(start_time, "\n")
   counter = 0;
 
   # read JSON and convert to data.frame row
@@ -95,19 +105,17 @@ colBindJson <- function(dataFrame) {
     if(length(dataList) == length(newList)) {
       if(dataList$reactionTime == "") {dataList$reactionTime <- NA}
       dataDf <- as.data.frame(matrix(unlist(dataList),nrow=length(dataList),byrow=TRUE))
-      # dataDf <- as.data.frame(do.call(rbind, dataList))
       # Transpose the list (which is interpreted as a column), to a row
       jsonData[rowNumber,] <- t(dataDf[1])
     } else {
       counter = counter + 1
     }
   }
-  # as.data.frame(lapply(jsonData, type.convert))
   jsonData <- as.data.frame(lapply(jsonData, type.convert, as.is = TRUE))
 
   combinedDF <- cbind(dataFrame, jsonData)
-  end_time <- format(Sys.time(), "%d-%b-%Y_%Hh%Mm%Ss")
-  cat(end_time, "\n")
+  # end_time <- format(Sys.time(), "%d-%b-%Y_%Hh%Mm%Ss")
+  # cat(end_time, "\n")
   if(counter > 0){
     cat(counter, "row(s) were removed. \n")
   }
@@ -115,29 +123,15 @@ colBindJson <- function(dataFrame) {
   return(combinedDF)
 }
 
-findLesson <- function(dataframe, lessonInfo) {
-  # dataframe[nrow(dataframe) + 1,] = list(1, 1, 1900, 1, "data", "1")
-
-
-  dataframe$lessonTitle <- lessonInfo$title[match(dataframe$lesson_id, lessonInfo$id)]
+findlesson <- function(dataframe, lessonInfo) {
+  dataframe$lessonTitle <- lessonInfo$title[match(dataframe$lessonId, lessonInfo$id)]
 
   return(dataframe)
 }
 
 findfact <- function(dataframe, factInfo) {
-  # dataframe[nrow(dataframe) + 1,] = list(1, 27666, 1900, 1, "data", "1")
-  #
-  # setkey(Employees,Department)
-  # setkey(Departments,Department)
-  # data.table::setkey(dataframe, fact_id)
-  # data.table::setkey(factInfo, id)
-  #
-  # dataframe[ , 'factText'] <- NA
-  #
-  # dataframe[lookup, on = paste0('fact_id', "==text"), (factText) := target]
-  #
-  #
-  # dataframe$lessonTitle <- lessonInfo$title[match(dataframe$lesson_id, lessonInfo$id)]
+  dataframe$factText <- factInfo$text[match(dataframe$factId, factInfo$id)]
+  dataframe$factAnswer <- factInfo$answer[match(dataframe$factId, factInfo$id)]
 
   return(dataframe)
 }
