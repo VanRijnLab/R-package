a76284d68ab719e07523c01d2e230616368d97f2
Han Havinga <h.m.havinga@gmail.com>
2022-06-16 13:10:54 +0200

    Replace sessionTime based code with presentationStartTime based

--- R/individual_ROF.R
+++ R/individual_ROF.R
@@ -19,7 +19,7 @@
 #' @param filepath A relative or explicit path where plots will be saved
 #' @return A preview plot in the viewer and a pdf file in filepath
 #' @export
-individual_ROF <- function(data, sessionId = NULL, normalizeTime = FALSE, xlim = NULL, ylim = NULL, filepath = NULL) {
+individual_ROF <- function(data, session = NULL, normalizeTime = FALSE, xlim = NULL, ylim = NULL, filepath = NULL) {
   if(missing(data)){
     stop("No data is provided")
   }
@@ -52,24 +52,23 @@ individual_ROF <- function(data, sessionId = NULL, normalizeTime = FALSE, xlim =
   # Single Session Settings
   sessionflag <- FALSE
 
-  if(is.null(sessionId)){
-    participants <- sort(unique(data$sessionId))
-  } else {
-    if(!(is.character(sessionId) & length(sessionId) == 1)){
-      stop("SessionId is not a string")
+  if(!is.null(session)){
+    if(!(is.character(session) & length(session) == 1)){
+      stop("Session is not a string")
     }
     sessionflag <- TRUE
-    participants <- character(0)
-    participants[1] <- sessionId
+    data <- dplyr::filter(data, sessionId == session)
+
   }
 
   # init plot lists
-  plot <- NULL
   plots <- list()
   plots4 <- list()
 
   # Determine axis
-  maxTime <- max(data$sessionTime)/60000
+  sessiongroup <- dplyr::group_by(data, sessionId)
+  sessiontimes <- dplyr::summarise(sessiongroup, times = (max(presentationStartTime, na.rm = TRUE) - min(presentationStartTime, na.rm = TRUE))/60000)
+  maxTime <- max(sessiontimes$times, na.rm = TRUE)
   if(is.null(xlim)){
     x = c(0, maxTime)
   } else {
@@ -82,39 +81,48 @@ individual_ROF <- function(data, sessionId = NULL, normalizeTime = FALSE, xlim =
   }
 
 
-
   cat("This may take a moment... \n")
-  # for (i in seq_along(participants)) {
-  #   dat1 <- dplyr::filter(data, sessionId == participants[i])
-  #
-  #   dat3 <- NULL
-  #   if(normalizeTime){
-  #     dat2 <- dplyr::group_by(dat1, factId)
-  #     dat3 <- dplyr::mutate(dat2, time = (sessionTime - min(sessionTime)) / 60000)
-  #     dat3 <- dplyr::ungroup(dat3)
-  #   } else {
-  #     dat3 <- dplyr::mutate(dat1, time = (sessionTime / 60000))
-  #   }
-  #   # Make plot title
-  #   lesson <- unique(substr(dat3$lessonTitle, 1, 19))
-  #   user <- unique(dat3$userId)
-  #   plotTitle <- paste("Lesson: ", lesson[1], ",User: ", user[1])
-  #
-  #   # Make plot
-  #   plot <- ggplot2::ggplot(data = dat3, ggplot2::aes(x = time, y = alpha)) +
-  #     ggplot2::geom_line(alpha = 1, ggplot2::aes(colour = factor(factId))) +
-  #     ggplot2::geom_point(alpha = 1, size = 1.5, stroke = 0, pch = 21, ggplot2::aes(fill = correct)) +
-  #     ggplot2::guides(colour = "none", fill = "none") +
-  #     ggplot2::scale_fill_manual(values = c("TRUE"="grey", "FALSE"= "red", "1"="grey", "0"= "red")) +
-  #     ggplot2::scale_color_manual(values = factcolor) +
-  #     ggplot2::coord_cartesian(xlim = x, ylim = y) +
-  #     ggplot2::labs(x = "Time (minutes)", y = "Alpha") +
-  #     ggplot2::ggtitle(plotTitle)
-  #   plots[[i]] <- plot
-  #   if(i < 5){
-  #     plots4[[i]] <- plot
-  #   }
-  # }
+
+
+  # Prepare data for graphs
+  data <- dplyr::filter(data, !is.na(alpha))
+  data$lessonTitle <- substr(data$lessonTitle, 1, 19)
+
+  datagroup <- dplyr::group_by(data, lessonId, userId)
+  datasort <- dplyr::arrange(datagroup, presentationStartTime)
+  timediff <- dplyr::mutate(datasort, breakTime = presentationStartTime - dplyr::lag(presentationStartTime))
+  sessiondiff <- dplyr::mutate(timediff, sessionOrder = cumsum(sessionId != dplyr::lag(sessionId, def = "none")))
+
+  full_data <- NULL
+  if(normalizeTime){
+    tinygroup <- dplyr::group_by(sessiondiff, lessonId, userId, sessionId, factId)
+    full_data <- dplyr::mutate(tinygroup, time = (presentationStartTime - min(presentationStartTime, na.rm = TRUE)) / 60000)
+  } else {
+    tinygroup <- dplyr::group_by(sessiondiff, lessonId, userId, sessionId)
+    full_data <- dplyr::mutate(tinygroup, time = (presentationStartTime - min(presentationStartTime, na.rm = TRUE)) / 60000)
+  }
+
+  full_data <- dplyr::ungroup(full_data)
+
+  split_data <- split(full_data, list(full_data$lessonId, full_data$userId, full_data$sessionId), drop = TRUE)
+
+  # Plot graphs
+  data_plots <- NULL
+  data_plots <- split_data %>% purrr::map(~ ggplot2::ggplot(data = ., ggplot2::aes(x = time, y = alpha)) +
+                                            ggplot2::geom_line(alpha = 1, ggplot2::aes(colour = factor(factId))) +
+                                            ggplot2::geom_point(alpha = 1, size = 1.5, stroke = 0, pch = 21, ggplot2::aes(fill = correct)) +
+                                            ggplot2::guides(colour = "none", fill = "none") +
+                                            ggplot2::scale_fill_manual(values = c("TRUE"="grey", "FALSE"= "red", "1"="grey", "0"= "red")) +
+                                            ggplot2::scale_color_manual(values = factcolor) +
+                                            ggplot2::coord_cartesian(xlim = x, ylim = y) +
+                                            ggplot2::labs(x = "Time (minutes)", y = "Alpha") +
+                                            ggplot2::ggtitle( label = paste("Lesson: ", .x$lessonTitle[1], ",User: ", .x$userId[1]),
+                                                              subtitle = paste("Session #", .x$sessionOrder[1], ",Since last session:", ms_to_string(.x$breakTime[1]) )))
+
+
+  plots <- as.list(data_plots)
+  plots <- plots[order(sapply(plots, function(x) x$data$userId[1]), sapply(plots, function(x) x$data$lessonId[1]), sapply(plots, function(x) x$data$sessionOrder[1]))]
+  plots4 <- plots[1:4]
 
   # Print plots
   res <- NULL
@@ -142,7 +150,6 @@ individual_ROF <- function(data, sessionId = NULL, normalizeTime = FALSE, xlim =
     cat("PDF of plots can be found in: ", fileplace, "\n")
   }
 
-  # Display first 4 plots
   return(res)
 
 
